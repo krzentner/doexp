57375ec2f699527183aa200337095ea37cb041f4
K.R. Zentner <krzentner@gmail.com>
2023-07-02 23:57:52 -0700

    Allow editable install

---
+++ setup.py
@@ -0,0 +1,10 @@
+# This setup.py exists purely to allow editable installs
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='doexp',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    scripts=['scripts/doexp']
+    )
