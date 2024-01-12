9a209a74ccc90ea5ba98e28418e4a87db6ff0a94
K.R. Zentner <krzentner@gmail.com>
2024-01-12 04:18:59 -0800

    Don't create out paths when printing a command

--- src/doexp/doexp.py
+++ src/doexp/doexp.py
@@ -275,19 +275,31 @@ def _sort_cmds(commands):
 
     return sorted(list(commands), key=key)
 
+def create_paths(cmd, data_dir, tmp_data_dir):
+    print('Creating paths')
+    for arg in cmd.args:
+        if isinstance(arg, In):
+            d = os.path.join(data_dir, arg.filename)
+            if not os.path.exists(d):
+                print(f'Missing In file {d}')
+        elif isinstance(arg, (Out, FileArg)):
+            # Use temporary directory here
+            d = os.path.join(tmp_data_dir, arg.filename)
+            extra = None
+            while not extra:
+                d, extra = os.path.split(d)
+            print(f"Creating directory ({d}) for Out file ({arg.filename}).")
+            os.makedirs(d, exist_ok=True)
+
 
 def _cmd_to_args(cmd, data_dir, tmp_data_dir):
     args = []
     for arg in cmd.args:
         if isinstance(arg, In):
-            assert not arg.filename.startswith("/")
             arg = os.path.join(data_dir, arg.filename)
-            os.makedirs(os.path.split(arg)[0], exist_ok=True)
         elif isinstance(arg, (Out, FileArg)):
-            assert not arg.filename.startswith("/")
             # Use temporary directory here
             arg = os.path.join(tmp_data_dir, arg.filename)
-            os.makedirs(os.path.split(arg)[0], exist_ok=True)
         args.append(str(arg))
     return args
 
@@ -297,7 +309,6 @@ def _filter_completed(running):
         p.proc.poll()
     completed = [p for p in running if p.proc.returncode is not None]
     now_running = [p for p in running if p not in completed]
-    assert len(completed) + len(now_running) == len(running)
     return now_running, completed
 
 
@@ -550,6 +561,7 @@ class Context:
 
     def _run_process(self, cmd, *, stdout, stderr):
         args = _cmd_to_args(cmd, self.data_dir, self._tmp_data_dir)
+        create_paths(cmd, self.data_dir, self._tmp_data_dir)
         env = os.environ.copy()
         cuda_devices = []
         for k, v in cmd.env:
